05251b6fed77debc2193d78787d893881cc6ff5b
sarazaToussaint <tmsaraza@gmail.com>
2023-04-06 22:02:29 +0200

    Remove the unused code

--- src/components/loginPage/Login.jsx
+++ src/components/loginPage/Login.jsx
@@ -74,8 +74,7 @@ const Login = () => (
           <div className="content">
             <h3>New here ?</h3>
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-              ex ratione.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             </p>
             <button type="button" className="btn transparent" id="sign-up-btn">
               Sign up

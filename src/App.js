bc0168ea4303832ff273c945543bd2a8b086f516
sarazaToussaint <tmsaraza@gmail.com>
2023-04-06 21:43:57 +0200

    Render the login page in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Login from './components/loginPage/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HOME PAGE</h1>
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
 

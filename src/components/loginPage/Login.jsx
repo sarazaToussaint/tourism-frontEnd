3240a5c203e1bc78e887d0c1d7a51abf4139c0b2
sarazaToussaint <tmsaraza@gmail.com>
2023-04-07 08:03:56 +0200

    Add the eventListener to all buttons

--- src/components/loginPage/Login.jsx
+++ src/components/loginPage/Login.jsx
@@ -1,71 +1,92 @@
-import React from 'react';
+import { React, useRef } from 'react';
 
-const Login = () => (
-  <>
-    <div className="container">
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form action="#" className="sign-in-form">
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
-              <i className="fas fa-user" />
-              <input type="text" placeholder="Username" />
+const Login = () => {
+  const containerRef = useRef(null);
+
+  const handleSignUpClick = () => {
+    containerRef.current.classList.add('sign-up-mode');
+  };
+
+  const handleSignInClick = () => {
+    containerRef.current.classList.remove('sign-up-mode');
+  };
+
+  return (
+    <>
+      <div className="container" ref={containerRef}>
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user" />
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock" />
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" className="btn solid" />
+              <p className="social-text">Or Sign in with social platforms</p>
+            </form>
+            <form action="#" className="sign-up-form">
+              <h2 className="title">Sign up</h2>
+              <div className="input-field">
+                <i className="fas fa-user" />
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope" />
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock" />
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" className="btn" value="Sign up" />
+              <p className="social-text">Or Sign up with social platforms</p>
+            </form>
+          </div>
+        </div>
+
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New here ?</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              </p>
+              <button
+                type="button"
+                className="btn transparent"
+                id="sign-up-btn"
+                onClick={handleSignUpClick}
+              >
+                Sign up
+              </button>
             </div>
-            <div className="input-field">
-              <i className="fas fa-lock" />
-              <input type="password" placeholder="Password" />
+            <img src="img/log.svg" className="image" alt="" />
+          </div>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>One of us ?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              </p>
+              <button
+                type="button"
+                className="btn transparent"
+                id="sign-in-btn"
+                onClick={handleSignInClick}
+              >
+                Sign in
+              </button>
             </div>
-            <input type="submit" value="Login" className="btn solid" />
-            <p className="social-text">Or Sign in with social platforms</p>
-          </form>
-          <form action="#" className="sign-up-form">
-            <h2 className="title">Sign up</h2>
-            <div className="input-field">
-              <i className="fas fa-user" />
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope" />
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock" />
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" className="btn" value="Sign up" />
-            <p className="social-text">Or Sign up with social platforms</p>
-          </form>
+            <img src="img/register.svg" className="image" alt="" />
+          </div>
         </div>
       </div>
-
-      <div className="panels-container">
-        <div className="panel left-panel">
-          <div className="content">
-            <h3>New here ?</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <button type="button" className="btn transparent" id="sign-up-btn">
-              Sign up
-            </button>
-          </div>
-          <img src="img/log.svg" className="image" alt="" />
-        </div>
-        <div className="panel right-panel">
-          <div className="content">
-            <h3>One of us ?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            </p>
-            <button type="button" className="btn transparent" id="sign-in-btn">
-              Sign in
-            </button>
-          </div>
-          <img src="img/register.svg" className="image" alt="" />
-        </div>
-      </div>
-    </div>
-  </>
-);
-
+    </>
+  );
+};
 export default Login;

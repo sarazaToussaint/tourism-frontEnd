f5bcd0e1b4317fd7f68e82236dd8c666b7684da9
sarazaToussaint <tmsaraza@gmail.com>
2023-04-14 22:13:37 +0200

    Create a file for connection

---
+++ src/components/Test.jsx
@@ -0,0 +1,21 @@
+import { useState, React } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import db from './firebase-config';
+
+const Test = () => {
+  const [newTest, setNewTest] = useState('');
+  const usersCollectionRef = collection(db, 'test_data');
+
+  const createTest = async () => {
+    await addDoc(usersCollectionRef, { test: newTest });
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Type your test name" onChange={(event) => setNewTest(event.target.value)} />
+      <button type="button" onClick={createTest}>Add</button>
+    </div>
+  );
+};
+
+export default Test;

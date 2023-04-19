ee054f3dad55828c99363e940c772101aae49af0
sarazaToussaint <tmsaraza@gmail.com>
2023-04-19 21:04:25 +0200

    Remove the unused codes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import Test from './components/Test';
 import Signin from './components/loginPage/Signin';
+// import './App.css';
+// import Login from './components/loginPage/Login';
 
 function App() {
   return (

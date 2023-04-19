b4a0dd2785dd3198b208933d497554bcd386e327
sarazaToussaint <tmsaraza@gmail.com>
2023-04-19 21:07:11 +0200

    Make changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import Test from './components/Test';
 import Signin from './components/loginPage/Signin';
+// import Test from './components/Test';
 // import './App.css';
 // import Login from './components/loginPage/Login';
 

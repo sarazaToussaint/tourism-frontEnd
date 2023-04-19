39fceaff9d7b54ae7ee24736496a2da85637ef11
sarazaToussaint <tmsaraza@gmail.com>
2023-04-19 21:02:25 +0200

    Make changes

--- src/components/loginPage/Signin.jsx
+++ src/components/loginPage/Signin.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { signInWithGoogle } from '../Firebase';
 
 const Signin = () => (
-  <div><button type="button" onClick={signInWithGoogle}>Sign In with Google</button></div>
+  <div>
+    <button type="button" onClick={signInWithGoogle}>Sign In with Google</button>
+  </div>
 );
 
 export default Signin;
